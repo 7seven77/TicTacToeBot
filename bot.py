02b5afed80fca5b94e0cc3c32931d92e6950047f
Zain Raja <zraja1105@gmail.com>
2020-07-13 23:42:34 +0100

    Add status command
    Calling the status command will show the current status of the game

--- bot.py
+++ bot.py
@@ -13,10 +13,20 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix=botPrefix)
 
+# Greets the user when they call it
 @bot.command(name='hello', help='Says hello!')
 async def hello(ctx):
     greetings = ["Hey {}!", "Hello {}", "Hey {}, how are you doing?",
                  "Yo {}", "Salam {}", "Allo {}"]
     await ctx.send(random.choice(greetings).format(ctx.author.mention));
 
+# Displays the status of the game
+@bot.command(name='status', help='Show the status of the game')
+async def status(ctx):
+    # If the value has not been set yet, a game is not being played
+    try:
+        if (board):
+            print("Value has been set");
+    except NameError:
+        await ctx.send("No game is being played")
 bot.run(TOKEN)
