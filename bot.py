2384d7b4efa4b3774439cbffa6a3963aa6833cfa
Zain Raja <zraja1105@gmail.com>
2020-07-13 23:23:26 +0100

    Add basic bot shell

---
+++ bot.py
@@ -0,0 +1,12 @@
+# bot.py
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='7')
+
+bot.run(TOKEN)
