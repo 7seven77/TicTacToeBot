45fc0a0a55b17af44063dd89101d07d0c68b7963
Zain Raja <zraja1105@gmail.com>
2020-07-13 23:27:43 +0100

    Add basic hellp command
    Add command that replies with a greeting when the user says hello

--- bot.py
+++ bot.py
@@ -4,9 +4,16 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# This is what a command must start with
+botPrefix = '7'
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='7')
+bot = commands.Bot(command_prefix=botPrefix)
+
+@bot.command(name='hello', help='Says hello!')
+async def hellp(ctx):
+    await ctx.send("Hey");
 
 bot.run(TOKEN)
